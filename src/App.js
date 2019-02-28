103bd6c14702e031b973a1057a20d88641efecad
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 23:19:34 +0000

    Added AddPlayerButton component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Player from './Molecules/Player';
+import AddPlayerButton from './Atoms/AddPlayerButton';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>PLAYERS</p>
-        <p>ADD PLAYER</p>
+        <AddPlayerButton />
         <p>RESET</p>
         <p>BOTTOM NAVIGATION</p>
+        <Player />
       </div>
     );
   }
