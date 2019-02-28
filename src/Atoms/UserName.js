24c52440efe427d17de2a96ec9f5aea359b0f4fd
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 23:59:42 +0000

    added minimal styling to UserName component

--- src/Atoms/UserName.js
+++ src/Atoms/UserName.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class UserName extends Component {
     render() {
         return (
-            <div>Johnny Doe
+            <div style={{fontSize:"1em", fontWeight: "bold"}}>Johnny Doe
                 {/*pass in name from parent component which holds the players objects*/}
             </div>
         );
