273d641d42c88a68d9bc535568d3058199e16a9d
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 13:56:41 +0000

    Added new Atom components

---
+++ src/Atoms/PointsInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PointsInput extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PointsInput;
