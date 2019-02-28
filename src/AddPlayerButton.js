7ee46cc4b8ebc16a3dead23265cea0680e951f94
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 12:14:38 +0000

    added AddPlayer pseudocode skeleton

---
+++ src/AddPlayerButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddPlayerButton extends Component {
+    render() {
+        return (
+            <div>
+                {/*Plus Icon with onClick event triggering the creation of a new Player component*/}
+            </div>
+        );
+    }
+}
+
+export default AddPlayerButton;
