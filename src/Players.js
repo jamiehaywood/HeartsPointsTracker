1e05dce888f2229450c52544a4f550b277c0a2c7
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 11:56:51 +0000

    Added psuedocode of Players component

---
+++ src/Players.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Players extends Component {
+    render() {
+        return (
+            <div>
+                {/* User Icon */}
+                {/* User Name */}
+                {/* Total Points */}
+                {/* Points Input */}
+            </div>
+        );
+    }
+}
+
+export default Players;
