cfd3ed463cc4afbe5a1b9042b6f785bea26f3ae7
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 14:45:20 +0000

    imported atom components into player molecule

--- src/Molecules/Player.js
+++ src/Molecules/Player.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import UserIcon from '../Atoms/UserIcon';
+import UserName from '../Atoms/UserName';
+import TotalPoints from '../Atoms/TotalPoints';
+import PointsInput from '../Atoms/PointsInput';
 
 class Player extends Component {
     render() {
         return (
             <div>
-                {/* User Icon */}
-                {/* User Name */}
-                {/* Total Points */}
-                {/* Points Input */}
+                <UserIcon />
+                <UserName />
+                <TotalPoints />
+                <PointsInput />
             </div>
         );
     }
