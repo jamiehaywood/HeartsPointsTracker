934f542a8045112b74a28478ae29af9db52d9938
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 23:37:01 +0000

    added userIconPlaceholder image

--- src/Atoms/UserIcon.js
+++ src/Atoms/UserIcon.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import userIconPlaceholder from './Images/userIconPlaceholder.jpg';
 class UserIcon extends Component {
     render() {
         return (
             <div>
+                <img src={userIconPlaceholder} alt="userIcon" height="65em"></img>
                 {/* Rendering an image of some sort.
                     1. Render a static image for now
                     2. Render an image uploaded by the user
