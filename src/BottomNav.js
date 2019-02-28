4ec242dde6011f6e5bd8a2d8be4a08814ce26680
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 12:17:38 +0000

    Added BottomNav pseudocode skeleton component

---
+++ src/BottomNav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BottomNav extends Component {
+    render() {
+        return (
+            <div>
+                {/*Most Points Per Game*/}
+                {/*Number of Games*/}
+                {/*Soundboard*/}
+            </div>
+        );
+    }
+}
+
+export default BottomNav;
