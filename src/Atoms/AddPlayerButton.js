5cd62c884cfed9de70cc24c9cd2319219fe30dfb
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 23:22:56 +0000

    updated AddPlayerButton component with image and minimal styling

--- src/Atoms/AddPlayerButton.js
+++ src/Atoms/AddPlayerButton.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import plus from "./Images/plus.png"
 
 class AddPlayerButton extends Component {
     render() {
         return (
             <div>
+                <img src={plus} height="60em" alt='plusIcon' onClick={{/*do something*/}
+                } style={{cursor:"pointer"}} />
+
                 {/*Plus Icon with onClick event triggering a "name" input popup and then a function that adds a new Player component*/}
             </div>
         );
