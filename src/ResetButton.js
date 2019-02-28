a0525d3d7f9c0a57e7a65cafcba683f56a37d5b3
Jamie Haywood <jamie.haywood@gmail.com>
2019-02-28 12:11:49 +0000

    added reset button skeleton

---
+++ src/ResetButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ResetButton extends Component {
+    render() {
+        return (
+            <div>
+                RESET
+            </div>
+        );
+    }
+}
+
+export default ResetButton;
