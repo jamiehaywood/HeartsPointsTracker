423c0bb8c2a0864b725ccb4971de443fc25e2dd9
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-01 00:17:35 +0000

    added placeholder onClick event to ResetButton component

--- src/Atoms/ResetButton.js
+++ src/Atoms/ResetButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ResetButton extends Component {
     render() {
         return (
-            <div style={{fontSize:"2em", fontWeight:"bold", cursor:"pointer"}}>
+            <div style={{fontSize:"2em", fontWeight:"bold", cursor:"pointer"}} onClick={{/*Clear parent array of player objects*/}}>
                 RESET{/* Reset clears all points arrays, and all users */}
             </div>
         );
