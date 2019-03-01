5095b996a272967687ac4a35a5675291b3d7a7fb
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-01 00:13:10 +0000

    added styling to ResetButton component

--- src/Atoms/ResetButton.js
+++ src/Atoms/ResetButton.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class ResetButton extends Component {
     render() {
         return (
-            <div>
-                {/* Reset clears all points arrays, and all users */}
+            <div style={{fontSize:"2em", fontWeight:"bold", cursor:"pointer"}}>
+                RESET{/* Reset clears all points arrays, and all users */}
             </div>
         );
     }
