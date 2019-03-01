b9be17d839d74ab1c5572545bfa5c6d6e1331435
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-01 15:17:59 +0000

    Created BottomNav component with atom components

--- src/Molecules/BottomNav.js
+++ src/Molecules/BottomNav.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import MostPointsPerGame from '../Atoms/MostPointsPerGame';
+import NumberOfGames from '../Atoms/NumberOfGames';
+import Soundboard from '../Atoms/SoundBoardIcon';
 
 class BottomNav extends Component {
     render() {
         return (
             <div>
-                {/*Most Points Per Game*/}
-                {/*Number of Games*/}
-                {/*Soundboard*/}
+                <MostPointsPerGame />
+                <NumberOfGames />
+                <Soundboard />
             </div>
         );
     }
