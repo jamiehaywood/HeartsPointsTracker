d04f72e3adcaccd8db609f84d9837cf6154860b5
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-02 21:41:25 +0000

    created a proper reset button, added styling

--- src/Atoms/ResetButton.js
+++ src/Atoms/ResetButton.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class ResetButton extends Component {
     render() {
         return (
-            <div style={{fontSize:"2em", fontWeight:"bold", cursor:"pointer"}} onClick={this.props.resetButton}>
-                RESET{/* Reset clears all points arrays, and all users */}
+            <div style={{textAlign:"center"}} >
+                <button type="button" onClick={this.props.resetButton} style={{fontSize:"2em", fontWeight:"bold", cursor:"pointer", textAlign:"center"}}>RESET</button>
+                
+                {/* Reset clears all points arrays, and all users */}
+
             </div>
         );
     }
