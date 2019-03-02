34bf44e78bcec96f0118ccbefbdad522229133f5
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-02 15:21:01 +0000

    Removed the addPlayer function and added the onClick event to the <div>

--- src/Atoms/AddPlayerButton.js
+++ src/Atoms/AddPlayerButton.js
@@ -2,26 +2,14 @@ import React, { Component } from 'react';
 import plus from "./Images/plus.png"
 
 class AddPlayerButton extends Component {
-    addPlayerFunction() {
-        var playerName = prompt("Please enter your name:", "Full Stack")
-
-        if (playerName === null || playerName === "") {
-            playerName = prompt("Please enter your name:", "You Bin");
-        }
-        else {
-            var obj = {};
-            obj["name"] = {playerName};
-            obj["points"] = [0,];
-            console.log(obj);
-            console.log("Player Object has been created");
-        }
-    }
     render() {
         return (
-            <div>
-                <img src={plus} height="60em" alt='plusIcon' onClick={this.addPlayerFunction} style={{cursor:"pointer"}} />
+            <div onClick={this.props.addPlayer}>
+                <img src={plus} height="60em" alt='plusIcon' style={{cursor:"pointer"}} />
 
-                {/*Plus Icon with onClick event triggering a "name" input popup and then a function that adds a new Player component*/}
+                {/* 1. Plus Icon with onClick event triggering a "name" input popup ✅
+                    2. A function that adds a new Player to the state array ✅
+                */}
             </div>
         );
     }
