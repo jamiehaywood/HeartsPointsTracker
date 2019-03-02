74634fe2a1120a0a92a71466dbe6c58d1f5d7fff
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-02 15:22:29 +0000

    Added state. addPlayerButton func added. Func props passed to child.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,41 @@ import Player from './Molecules/Player';
 import ResetButton from './Atoms/ResetButton';
 import BottomNav from './Molecules/BottomNav';
 
-
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      playersData : []
+  }
+    this.addPlayerButton = this.addPlayerButton.bind(this)
+  }
+
+  addPlayerButton (){
+    var playerName = prompt("Please enter your name:", "")
+
+        if (playerName === null || playerName === "") {
+            playerName = prompt("Please enter your name:", "");
+        }
+        else {
+            var obj = {};
+            obj["name"] = playerName;
+            obj["points"] = [0,];
+            console.log("Player Object has been created");
+            console.log(obj)
+        }
+        this.setState(this.state)
+        var playersData = this.state.playersData
+        playersData.push(obj)
+        console.log(playersData)
+        return {
+            playersData: playersData
+        }
+      }
   render() {
     return (
       <div>
         <Player />
-        <AddPlayerButton />
+        <AddPlayerButton addPlayerButton={this.addPlayerButton} />
         <ResetButton />
         <BottomNav />
       </div>
