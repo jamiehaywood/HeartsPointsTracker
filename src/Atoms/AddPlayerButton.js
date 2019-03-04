533c21547fb7314021559a63ae9970f48e85a430
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-04 20:15:39 +0000

    added padding to plus button, cleaned up pseudo code

--- src/Atoms/AddPlayerButton.js
+++ src/Atoms/AddPlayerButton.js
@@ -4,12 +4,8 @@ import plus from "./Images/plus.png"
 class AddPlayerButton extends Component {
     render() {
         return (
-            <div onClick={this.props.addPlayerButton} style={{textAlign:"center"}}>
+            <div onClick={this.props.addPlayerButton} style={{textAlign:"center", padding:"10px"}}>
                 <img src={plus} height="60em" alt='plusIcon' style={{cursor:"pointer"}} />
-
-                {/* 1. Plus Icon with onClick event triggering a "name" input popup ✅
-                    2. A function that adds a new Player to the state array ✅
-                */}
             </div>
         );
     }
