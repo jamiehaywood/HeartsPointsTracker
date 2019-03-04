f4e2c1d333d4418158f6887db214b7b931402821
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-04 17:01:19 +0000

    created an enterKeyPress function passing points and name to addToPoints

--- src/Molecules/Player.js
+++ src/Molecules/Player.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import UserIcon from '../Atoms/UserIcon';
 
 class Player extends Component {
+    enterKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            var name = this.props.name;
+            var points = document.getElementById(this.props.name).value
+          this.props.addToPoints(name, points)
+          document.getElementById(this.props.name).value = ""
+        }
+      }
     render() {
         return (
             <div style={{textAlign:"center", borderBottom:"1px solid #d0d0d0"}}>
@@ -10,7 +18,7 @@ class Player extends Component {
                     <UserIcon />
                     {this.props.name}
                 </div>
-                <div><input type="number" name="Input Points"/></div>
+                <input type="number" id={this.props.name} onKeyPress={this.enterKeyPress}/>
                 <div style={{width:"65px", fontSize:"30px"}}><b>{this.props.points}</b></div>
             </div>
             </div>
