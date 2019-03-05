3f0c062b0973d394f1cd6084ff5d0b21bbbd7031
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-05 21:43:10 +0000

    added padding to reset button

--- src/Atoms/ResetButton.js
+++ src/Atoms/ResetButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ResetButton extends Component {
     render() {
         return (
-            <div style={{textAlign:"center"}} >
+            <div style={{textAlign:"center", paddingRight:"20px"}} >
                 <button type="button" onClick={this.props.resetButton} style={{fontSize:"2em", fontWeight:"bold", cursor:"pointer", textAlign:"center"}}>RESET</button>
             </div>
         );
