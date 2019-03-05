ce3539eece088afb2ce31710963f2ad1ff6994df
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-05 16:39:35 +0000

    removed components, updated styling and passed number of games props

--- src/Molecules/BottomNav.js
+++ src/Molecules/BottomNav.js
@@ -4,9 +4,8 @@ import Soundboard from '../Atoms/SoundBoardIcon';
 class BottomNav extends Component {
     render() {
         return (
-            <div>
-                <MostPointsPerGame />
-                <NumberOfGames />
+            <div style={{display:"flex", alignItems:"center", justifyContent:"space-evenly", backgroundColor:"#F6F6F6", position:"fixed", bottom:"0%", width:"100%"}}>
+                <div style={{fontSize:"30px"}}><b>{this.props.gamesNumber}</b></div>
                 <Soundboard />
             </div>
         );
