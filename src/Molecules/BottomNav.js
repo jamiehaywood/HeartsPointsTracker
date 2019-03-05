200fd6c56fc78c25b9b4512384929de7062d74d2
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-05 21:55:19 +0000

    styling changes

--- src/Molecules/BottomNav.js
+++ src/Molecules/BottomNav.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Soundboard from '../Atoms/SoundBoardIcon';
+import SoundBoardIcon from '../Atoms/SoundBoardIcon';
 
 class BottomNav extends Component {
     render() {
         return (
-            <div style={{display:"flex", alignItems:"center", justifyContent:"space-evenly", backgroundColor:"#F6F6F6", position:"fixed", bottom:"0%", width:"100%"}}>
-                <div style={{fontSize:"30px"}}><b>{this.props.gamesNumber}</b></div>
-                <Soundboard />
+            <div style={{display:"flex", alignItems:"center", justifyContent:"space-between", backgroundColor:"#F6F6F6", width:"100%", borderTop:"1px solid #D0D0D0"}}>
+                
+                <div style={{textAlign: "center", padding:"0px 20px"}}>
+                    <div style={{fontSize:"25px", color:"#686868"}}><b>{this.props.gamesNumber}</b></div>
+                    <b style={{fontSize:"12px", color:"#686868"}}>№ Games</b>
+                </div>
+
+            <SoundBoardIcon />
             </div>
         );
     }
