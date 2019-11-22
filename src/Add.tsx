49c07042471685bd43063b6ffd48199ae6a16521
Jamie Haywood <jamie.haywood@gmail.com>
2019-11-22 23:40:12 +0000

    created add button

---
+++ src/Add.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Player } from './Player/Player'
+
+interface AddProps {
+    currentPlayers: never[]
+    addPlayer: Function
+}
+
+export const Add: React.FC<AddProps> = (props) => {
+    function addPlayer() {
+        var playerName = prompt('Please enter your name:', '')!
+        if (playerName == null) { return }
+        props.addPlayer(([...props.currentPlayers, <Player name={playerName} />]))
+    }
+
+    return (
+        <div style={{ textAlign: 'center', padding: '10px' }}>
+            <img onClick={addPlayer} src={"https://image.flaticon.com/icons/svg/25/25304.svg"} height='60em' alt='plusIcon' style={{ cursor: 'pointer' }} />
+        </div>
+    )
+}
