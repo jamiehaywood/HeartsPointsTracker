2f178eec3dd32c1c034a70825cab80941b7ef322
Jamie Haywood <jamie.haywood@gmail.com>
2019-11-22 23:41:08 +0000

    created input with mobile onBlur workaround

---
+++ src/Player/Input.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import '../css/player.css'
+
+interface props {
+    name: string
+    setPoints: Function
+    points: number
+}
+
+const Input: React.FC<props> = (props) => {
+    const displayPoints = () => {
+        (document.getElementById(props.name) as HTMLInputElement).value = props.points.toString()
+    }
+
+    const addPoints = () => {
+        let inputPoints = (document.getElementById(props.name) as HTMLInputElement).value
+        let added = (props.points + parseInt(inputPoints))
+        props.setPoints(added);
+    }
+
+    useEffect(() => {
+        displayPoints()
+    })
+
+    // This is to prevent adding twice when you press enter and then 'onBlur'
+    const [arePointsSet, pointsAreSet] = useState(false)
+
+    return (
+        <input className="input" type="tel" id={props.name}
+            onFocus={(e) => { (document.getElementById(props.name) as HTMLInputElement).value = '' }}
+            onKeyPress={
+                (e) => {
+                    if (e.key === 'Enter') {
+                        pointsAreSet(true);
+                        addPoints()
+                        e.preventDefault()
+                    }
+
+                }}
+            onBlur={(e: any) => {
+                if (!arePointsSet) {
+                    addPoints();
+                    pointsAreSet(false)
+                }
+                displayPoints()
+            }
+            }
+        />
+    )
+}
+export default Input
