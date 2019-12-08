aa7372afbb9298be9bc2ea335d0a1f5e99b2617e
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-08 13:01:14 +0000

    refactored

--- src/Player/Player.tsx
+++ src/Player/Player.tsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Name from './Name'
 import Input from './Input'
 import PlayerIcon from './PlayerIcon'
 import '../css/player.css'
 
 interface PlayerProps {
-    name: string
+    name: string;
+    points: number;
+    setPoints: Function;
 }
 
 export const Player: React.FC<PlayerProps> = (props) => {
-    useEffect(() => { console.log(points) })
-    const [points, setPoints] = useState(0)
 
     return (
         <div className="playercontainer">
-                <PlayerIcon />
-                <Name name={props.name} />
-                <Input points={points} setPoints={setPoints} name={props.name} />   
+            <PlayerIcon />
+            <Name name={props.name} />
+            <Input points={props.points} setPoints={props.setPoints} name={props.name} />
         </div>
     );
 }
