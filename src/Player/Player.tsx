4294e2815cc6f06820c154df9d89d935575b63a4
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-08 22:02:02 +0000

    added set image and image

--- src/Player/Player.tsx
+++ src/Player/Player.tsx
@@ -8,13 +8,15 @@ interface PlayerProps {
     name: string;
     points: number;
     setPoints: Function;
+    setImage: Function;
+    image?: File | undefined
 }
 
 export const Player: React.FC<PlayerProps> = (props) => {
 
     return (
         <div className="playercontainer">
-            <PlayerIcon />
+            <PlayerIcon setImage={props.setImage} image={props.image} />
             <Name name={props.name} />
             <Input points={props.points} setPoints={props.setPoints} name={props.name} />
         </div>
