057ea7739dbebbbce4c4d2e65fd47215b033674b
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-08 22:01:53 +0000

    refactored to use store

--- src/Player/PlayerIcon.tsx
+++ src/Player/PlayerIcon.tsx
@@ -2,19 +2,28 @@ import React, { useState } from 'react'
 import { squid } from '../images'
 import '../css/player.css'
 
-const PlayerIcon = () => {
-    const [image, setImage] = useState()
-    let key = Math.random().toString()
+interface props {
+    image?: File | undefined
+    setImage: Function
+}
+let key = Math.random().toString()
+
+const PlayerIcon: React.FC<props> = (props) => {
     return (
         <div className="alignleft image">
-            <input className="visually-hidden" type="file" id={key} onChange={(e) => {
-                let file = e.target.files![0]
-                setImage(file)
-            }} />
-            {image
-            // eslint-disable-next-line
-            ? <img className="uploaded" src={URL.createObjectURL(image)} onClick={() => document.getElementById("myfile")!.click()} />
-            // eslint-disable-next-line
+            <input
+                className="visually-hidden"
+                type="file"
+                onChange={(e: any) => {
+                    props.setImage(e)
+                }}
+                id={key}
+            />
+
+            {props.image
+                // eslint-disable-next-line
+                ? <img className="uploaded" src={URL.createObjectURL(props.image)} onClick={() => document.getElementById("myfile")!.click()} />
+                // eslint-disable-next-line
                 : <img src={squid} onClick={() => document.getElementById(key)!.click()} />}
 
         </div>
