aa19dce74fc33ff219302db3e8033eaccb0fc986
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-08 12:45:31 +0000

    refactor to use store

--- src/Add.tsx
+++ src/Add.tsx
@@ -1,25 +1,14 @@
-import React from 'react'
-import { Player } from './Player/Player'
+import React, { useContext } from 'react'
+import { observer } from 'mobx-react';
+import GlobalStore from './index'
 
-interface AddProps {
-    currentPlayers: never[]
-    addPlayer: Function
-}
 
-export const Add: React.FC<AddProps> = (props) => {
-    function addPlayer() {
-        var playerName = prompt('Please enter your name:', '')!
-        if (playerName) {
-            props.addPlayer(([...props.currentPlayers, <Player name={playerName} />]))
-        }
-        else {
-            return
-        }
-    }
+export const Add: React.FC = observer(() => {
+    const store = useContext(GlobalStore)
 
     return (
         <div style={{ textAlign: 'center', padding: '10px' }}>
-            <img onClick={addPlayer} src={"https://image.flaticon.com/icons/svg/25/25304.svg"} height='60em' alt='plusIcon' style={{ cursor: 'pointer' }} />
+            <img onClick={() => { store.Players.addPlayer() }} src={"https://image.flaticon.com/icons/svg/25/25304.svg"} height='60em' alt='plusIcon' style={{ cursor: 'pointer' }} />
         </div>
     )
-}
+})
