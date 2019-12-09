1c4f06ca2851660ed7324dc2a47ddfd0e8e4bfef
Jamie Haywood <jamiehaywood1@tfl.gov.uk>
2019-12-09 09:46:06 +0000

    added conditional rendering for resume

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './css/navbar.css'
-import { saveIcon, sound } from './images'
+import { saveIcon, sound, resume } from './images'
+import GlobalStore from './index'
 
 export const NavBar = () => {
-
+    const store = useContext(GlobalStore)
     return (
         <div className="navbar" onClick={() => { console.log("hi"); }}>
             <div style={{ textAlign: 'center', padding: '0px 20px' }}>
-                <div style={{ fontSize: '25px', color: '#686868' }}><b>{10}</b></div>
+                <div style={{ fontSize: '25px', color: '#686868' }}><b>{store.GameStore.numberOfGames}</b></div>
                 <b style={{ fontSize: '12px', color: '#686868' }}>№ Games</b>
             </div>
-            <div style={{ margin: "auto" }}><img alt="hi" src={saveIcon} /></div>
+            {store.Players.players[0]
+                ? <div style={{ margin: "auto" }}><img alt="hi" src={saveIcon} /></div>
+                : <div style={{ margin: "auto" }}><img alt="hi1" src={resume} /></div>}
             <div style={{ padding: "0px 20px" }}><img alt="ho" src={sound} /></div>
-
         </div>
     )
 }
