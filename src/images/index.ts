661fecab5675a1ce415924467ba4b3ef89b14a20
Jamie Haywood <jamiehaywood1@tfl.gov.uk>
2019-12-09 09:47:11 +0000

    added resume icon

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,4 +1,5 @@
 const squid = require('./squid.svg') as string;
 const saveIcon = require('./save.svg') as string;
 const sound = require('./sound.svg') as string;
-export { squid, saveIcon, sound }
+const resume = require('./resume.svg') as string;
+export { squid, saveIcon, sound, resume }
