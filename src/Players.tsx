26f1ff31d26fc53a26a5e8cb13ba38c9cd3aa4f8
Jamie Haywood <jamiehaywood1@tfl.gov.uk>
2019-12-11 13:10:22 +0000

    added sort to players based on points

--- src/Players.tsx
+++ src/Players.tsx
@@ -1,23 +1,24 @@
-import React, { useContext } from 'react'
-import GlobalStore from './index'
-import { Player } from './Player/Player'
-import { observer } from 'mobx-react'
-
+import React, { useContext } from "react";
+import GlobalStore from "./index";
+import { Player } from "./Player/Player";
+import { observer } from "mobx-react";
 
 export const Players: React.FC = observer(() => {
-    const store = useContext(GlobalStore).Players
-    return (
-        <div>
-            {store.players.map((player, i) =>
-                <Player
-                    name={player.name}
-                    points={player.points}
-                    setPoints={player.addPoints}
-                    key={i}
-                    setImage={player.changeImage}
-                    image={player.image}
-                />
-            )}
-        </div>
-    )
-})
+  const store = useContext(GlobalStore).Players;
+  return (
+    <div>
+      {store.players
+        .sort((a, b) => (a.points > b.points ? -1 : 1))
+        .map((player, i) => (
+          <Player
+            name={player.name}
+            points={player.points}
+            setPoints={player.addPoints}
+            key={i}
+            setImage={player.changeImage}
+            image={player.image}
+          />
+        ))}
+    </div>
+  );
+});
