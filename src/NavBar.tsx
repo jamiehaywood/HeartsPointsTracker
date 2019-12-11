6de0f058aba14338512e965594f4618985069072
Jamie Haywood <jamiehaywood1@tfl.gov.uk>
2019-12-11 13:11:54 +0000

    added observer to check if game has started

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -2,19 +2,20 @@ import React, { useContext } from 'react'
 import './css/navbar.css'
 import { saveIcon, sound, resume } from './images'
 import GlobalStore from './index'
+import { observer } from 'mobx-react'
 
-export const NavBar = () => {
-    const store = useContext(GlobalStore)
-    return (
-        <div className="navbar" onClick={() => { console.log("hi"); }}>
-            <div style={{ textAlign: 'center', padding: '0px 20px' }}>
-                <div style={{ fontSize: '25px', color: '#686868' }}><b>{store.GameStore.numberOfGames}</b></div>
-                <b style={{ fontSize: '12px', color: '#686868' }}>№ Games</b>
-            </div>
-            {store.Players.players[0]
-                ? <div style={{ margin: "auto" }}><img alt="hi" src={saveIcon} /></div>
-                : <div style={{ margin: "auto" }}><img alt="hi1" src={resume} /></div>}
-            <div style={{ padding: "0px 20px" }}><img alt="ho" src={sound} /></div>
-        </div>
-    )
-}
+export const NavBar = observer(() => {
+  const store = useContext(GlobalStore)
+  return (
+    <div className="navbar" onClick={() => { console.log("hi"); }}>
+      <div style={{ textAlign: 'center', padding: '0px 20px' }}>
+        <div style={{ fontSize: '25px', color: '#686868' }}><b>{store.GameStore.numberOfGames}</b></div>
+        <b style={{ fontSize: '12px', color: '#686868' }}>№ Games</b>
+      </div>
+      {store.Players.players.length > 0
+        ? <div style={{ margin: "auto" }}><img alt="hi" src={saveIcon} /></div>
+        : <div style={{ margin: "auto" }}><img alt="hi1" src={resume} /></div>}
+      <div style={{ padding: "0px 20px" }}><img alt="ho" src={sound} /></div>
+    </div>
+  )
+})
