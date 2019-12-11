91280be4bbaefc825caf5ad50b3f81e0aeb52078
Jamie Haywood <jamiehaywood1@tfl.gov.uk>
2019-12-11 13:11:15 +0000

    refactored Add.tsx to look cleaner

--- src/Add.tsx
+++ src/Add.tsx
@@ -1,14 +1,19 @@
-import React, { useContext } from 'react'
-import { observer } from 'mobx-react';
-import GlobalStore from './index'
-
+import React, { useContext } from "react";
+import { observer } from "mobx-react";
+import GlobalStore from "./index";
 
 export const Add: React.FC = observer(() => {
-    const store = useContext(GlobalStore)
+  const store = useContext(GlobalStore);
 
-    return (
-        <div style={{ textAlign: 'center', padding: '10px' }}>
-            <img onClick={() => { store.Players.addPlayer() }} src={"https://image.flaticon.com/icons/svg/25/25304.svg"} height='60em' alt='plusIcon' style={{ cursor: 'pointer' }} />
-        </div>
-    )
-})
+  return (
+    <div style={{ textAlign: "center", padding: "10px" }}>
+      <img
+        onClick={() => { store.Players.addPlayer(); }}
+        src={"https://image.flaticon.com/icons/svg/25/25304.svg"}
+        height="60em"
+        alt="plusIcon"
+        style={{ cursor: "pointer" }}
+      />
+    </div>
+  );
+});
