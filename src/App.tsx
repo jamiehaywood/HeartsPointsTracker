5c194100bd4e8447d2c3c3dc1124cd41fd2e8534
Jamie Haywood <jamiehaywood1@tfl.gov.uk>
2019-12-11 13:11:35 +0000

    applied prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import './css/App.css';
+import React from "react";
+import "./css/App.css";
 
-import { Players } from './Players';
-import { Add } from './Add';
-import { NavBar } from './NavBar';
-
+import { Players } from "./Players";
+import { Add } from "./Add";
+import { NavBar } from "./NavBar";
 
 export interface PlayersContextInterface {
-  name: string
-  points: number
-  gamesPlayed: number
-  image: File | undefined
+  name: string;
+  points: number;
+  gamesPlayed: number;
+  image: File | undefined;
 }
-
-
 
 export const App = () => {
   return (
     <div>
-      <div style={{paddingBottom:"8vh", overflow: "scroll" }}>
+      <div style={{ paddingBottom: "8vh", overflow: "scroll" }}>
         <Players />
         <Add />
       </div>
       <NavBar />
     </div>
-  )
+  );
 };
 
 export default App;
