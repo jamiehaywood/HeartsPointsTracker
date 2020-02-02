3e02795fdb4f8ca5c8c583efdd07aee84aded319
Jamie Haywood <jamie.haywood@gmail.com>
2020-02-02 17:01:05 +0000

    added fontSize calculation for name

--- src/Player/Name.tsx
+++ src/Player/Name.tsx
@@ -7,7 +7,12 @@ interface props {
 
 const Name: React.FC<props> = (props) => {
     function calculateFontSize(name: string) {
-        return name.length > 10 ? "6vw" : "8vw"
+        // If the name is split with a space, then can fit over two lines
+        if (name.includes(' ')) {
+            return "8vw"
+        }
+        // else name needs to be shrunk to keep on one line
+        else return name.length > 10 ? "6vw" : "8vw"
     }
 
     const style = {
