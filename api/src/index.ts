23151c21a96a4043761ab4acda7f44cebf7cb155
Jamie Haywood <jamie.haywood@gmail.com>
2021-05-16 20:15:25 +0100

    refactor: extract start to startup file ♻️

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,3 @@
-import express from 'express';
+import { start } from './server/startup.server';
 
-const app = express();
-const port = 8080;
-
-app.get('/api/ping', (req, res) => {
-  res.json({ data: 'pong from mono-api 👋🏓' });
-});
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+start();
