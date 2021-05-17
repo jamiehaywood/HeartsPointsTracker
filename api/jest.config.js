65a9a1b27db4d1f98b3acb970581f305ed38e831
Jamie Haywood <jamie.haywood@gmail.com>
2021-05-17 19:35:12 +0100

    chore: update & add configs 🔧

---
+++ api/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coveragePathIgnorePatterns: [
+    'src/interfaces/',
+    'src/enums',
+    'src/certs',
+    'src/index.ts',
+    'src/routes/index.ts',
+    'src/server/startup.server.ts',
+  ],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testTimeout: 10000,
+};
