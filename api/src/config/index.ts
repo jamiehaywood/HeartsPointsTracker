6d0185af5c3ba9dbce114e589ea4a0ba26f31252
Jamie Haywood <jamie.haywood@gmail.com>
2021-06-18 08:36:11 +0100

    refactor: update env config file ♻️

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -6,26 +6,26 @@ import { production } from './production';
 const env = process.env.NODE_ENV || 'development';
 
 type EnvBaseConfig = {
+  MONGODB_URI: string;
   NODE_ENV: string;
   PORT: string;
-  MONGODB_URI: string;
   jwt: {
-    JWT_AUTH_SECRET: string;
     JWT_AUTH_EXPIRES: string;
-    JWT_REFRESH_SECRET: string;
+    JWT_AUTH_SECRET: string;
     JWT_REFRESH_EXPIRES: string;
+    JWT_REFRESH_SECRET: string;
   };
 };
 // This holds values that are consistent across all platforms
 const baseConfig: Partial<EnvBaseConfig> = {
+  MONGODB_URI: process.env.MONGODB_URI,
   NODE_ENV: env,
   PORT: process.env.PORT,
-  MONGODB_URI: process.env.MONGODB_URI,
   jwt: {
-    JWT_AUTH_SECRET: '',
     JWT_AUTH_EXPIRES: (60 * 60 * 24 * 1).toString(),
-    JWT_REFRESH_SECRET: '',
+    JWT_AUTH_SECRET: '',
     JWT_REFRESH_EXPIRES: (60 * 60 * 24 * 7).toString(),
+    JWT_REFRESH_SECRET: '',
   },
 };
 
